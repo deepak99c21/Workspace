4266cec312b4f559a9698d261c32f850f34842ca
Deepak <deepakpatra@gmail.com>
2025-07-14 10:31:36 +0530

    no commit ..

--- MyLearning/CPP/LambdaTest.cpp
+++ MyLearning/CPP/LambdaTest.cpp
@@ -5,17 +5,18 @@ using namespace std;
 int main()
 {
 	int x = 10; int y = 10;
-	auto l = [x, &y] {
+	auto l = [x, &y] ()->void{
 		//x++;
 		y++;
 		cout << "x: " << x << ", y: " << y << endl;
 		};
-	l();
+l();
 	x = 100;
 	y = 100;
 	l();
 	cout << "x: " << x << ", y: " << y << endl;
-	//Comment to check how it looks on git 
-	return 0;
+//	//Comment to check how it looks on git 
+
+return 0;
 
 }
